33cf430ddaf3e6e275e3b4cca1d6bc06d1533f32
sakth <sakth@LAPTOP-PH4T5HGA.home>
2022-10-28 17:56:25 +0100

    fst commit

--- E2EProject/src/main/java/com/e2e/pageobject/IndexPage.java
+++ E2EProject/src/main/java/com/e2e/pageobject/IndexPage.java
@@ -9,7 +9,7 @@ import com.e2e.testbase.BaseClass;
 
 public class IndexPage extends BaseClass {
 
-	// page factory metho
+	// page factory method
 	@FindBy(xpath = "//a[contains(text(),'My Account')]")
 	WebElement signInBtn;
 
